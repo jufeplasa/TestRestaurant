5a24145a4e8d623a50e71f6554a0bd1dc2568271
Juan Felipe <jufeplasa@hotmail.com>
2021-10-07 01:18:09 -0500

    second Commit

--- TestRestaurant/src/model/Recipe.java
+++ TestRestaurant/src/model/Recipe.java
@@ -4,16 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Recipe {
-	
+
 	private List<Ingredient> ingredients;
-	
+
 	public Recipe() {
 		ingredients=new ArrayList<Ingredient>();
 	}
-	
+
 	public void addIngredient(String n, double w) {
-		Ingredient newIngredient=new Ingredient(n,w);
-		ingredients.add(newIngredient);
+		boolean find=false;
+		for(int i=0;i<ingredients.size() && !find;i++) {
+			if(n.equalsIgnoreCase(ingredients.get(i).getName())) {
+				ingredients.get(i).addWeight(w);
+				find=true;
+			}
+		}
+		if(!find) {
+			Ingredient newIngredient=new Ingredient(n,w);
+			ingredients.add(newIngredient);
+		}
 	}
 
 	public List<Ingredient> getIngredients() {
--- TestRestaurant/test/model/RecipeTest.java
+++ TestRestaurant/test/model/RecipeTest.java
@@ -2,6 +2,8 @@ package model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 class RecipeTest {
@@ -18,18 +20,46 @@ class RecipeTest {
 	}
 	
 	@Test
-	void testAddIngredient() {
+	public void testAddIngredient() {
 		setupStage1();
+		String n="Sal";
+		double w=12;
+		rec.addIngredient(n, w);
+		List<Ingredient> ing= rec.getIngredients();
+		assertEquals(1,ing.size());
+		assertEquals("Sal", rec.getIngredients().get(0).getName());
+		assertEquals(12, rec.getIngredients().get(0).getWeight());
 	}
 	
 	@Test
-	void testAddIngredient2() {
+	public void testAddIngredient2() {
 		setupStage2();
+
+		String n="Pimienta";
+		double w=6;
+		rec.addIngredient(n, w);
+		
+		List<Ingredient> ing= rec.getIngredients();
+		assertEquals(4,ing.size());
+		
+		assertEquals("Pimienta", rec.getIngredients().get(rec.getIngredients().size()-1).getName());
+		assertEquals(6, rec.getIngredients().get(rec.getIngredients().size()-1).getWeight());
 	}
 	
 	@Test
-	void testAddIngredient3() {
+	public void testAddIngredient3() {
 		setupStage2();
+		
+		String n="Ajo";
+		double w=21;
+		rec.addIngredient(n, w);
+		
+		List<Ingredient> ing= rec.getIngredients();
+		assertEquals(3,ing.size());
+		
+
+		assertEquals("Ajo", rec.getIngredients().get(1).getName());
+		assertEquals(79, rec.getIngredients().get(1).getWeight());
 	}
 
 }
